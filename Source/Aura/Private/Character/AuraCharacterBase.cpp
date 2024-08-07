d44166b1b81a14a75ec8986924f180cdfdde90b0
jinkunou <743144825@qq.com>
2024-08-07 23:45:12 +0800

    Player and Enemy Characters.

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+// Copyright AnnihilateSword
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright AnnihilateSword
+
+
+#include "Character/AuraEnemy.h"
+
